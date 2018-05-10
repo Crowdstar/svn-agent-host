0f7ab8e43784a6634698fb9d001bd475ba4c62bb
Demin Yin <demin.yin@glu.com>
2018-05-09 17:57:57 -0700

    check if path is passed in before running SVN checkout

--- src/CrowdStar/SVNAgent/Actions/Checkout.php
+++ src/CrowdStar/SVNAgent/Actions/Checkout.php
@@ -16,28 +16,33 @@ class Checkout extends AbstractAction
      */
     public function processAction(): AbstractAction
     {
-        $url = $this->getConfig()->getSvnRoot() . $this->getRequest()->get('path');
-        $dir = $this->getConfig()->getSvnRootDir() . $this->getRequest()->get('path');
-        if (!is_dir($dir)) {
-            if (!is_dir(dirname($dir))) {
-                mkdir(dirname($dir), 0755, true);
-            }
-
-            $this->setMessage('SVN checkout')->exec(
-                function () use ($url, $dir) {
-                    sh::svn(
-                        'checkout',
-                        '--username',
-                        $this->getRequest()->getUsername(),
-                        '--password',
-                        $this->getRequest()->getPassword(),
-                        $url,
-                        $dir
-                    );
+        $path = trim($this->getRequest()->get('path'));
+        if (!empty($path)) {
+            $url = $this->getConfig()->getSvnRoot() . $path;
+            $dir = $this->getConfig()->getSvnRootDir() . $path;
+            if (!is_dir($dir)) {
+                if (!is_dir(dirname($dir))) {
+                    mkdir(dirname($dir), 0755, true);
                 }
-            );
+
+                $this->setMessage('SVN checkout')->exec(
+                    function () use ($url, $dir) {
+                        sh::svn(
+                            'checkout',
+                            '--username',
+                            $this->getRequest()->getUsername(),
+                            '--password',
+                            $this->getRequest()->getPassword(),
+                            $url,
+                            $dir
+                        );
+                    }
+                );
+            } else {
+                $this->setError("Folder '{$dir}' already exists");
+            }
         } else {
-            $this->setError("Folder '{$dir}' already exists");
+            $this->setError('Path not passed in when trying to do SVN checkout');
         }
 
         return $this;
