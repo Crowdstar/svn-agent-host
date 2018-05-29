4209dc1c91502a2beccb3e6f9882f40abddbef21
Demin Yin <demin.yin@glu.com>
2018-05-29 10:52:57 -0700

    checkout folder locally after being created in SVN

--- src/CrowdStar/SVNAgent/Actions/Create.php
+++ src/CrowdStar/SVNAgent/Actions/Create.php
@@ -19,9 +19,10 @@ class Create extends AbstractAction
     public function processAction(): AbstractAction
     {
         $url = $this->getSvnUri();
+        $dir = $this->getSvnDir();
         if (!SVNHelper::urlExists($url, $this->getRequest())) {
             $this->setMessage('SVN mkdir')->exec(
-                function () use ($url) {
+                function () use ($url, $dir) {
                     ShellWrap::svn(
                         'mkdir',
                         $url,
@@ -32,12 +33,27 @@ class Create extends AbstractAction
                             'm'        => 'path added through SVN Agent',
                         ]
                     );
+                },
+                function () use ($url, $dir) {
+                    if (!is_dir(dirname($dir))) {
+                        mkdir(dirname($dir), 0755, true);
+                    }
+
+                    ShellWrap::svn(
+                        'checkout',
+                        '--username',
+                        $this->getRequest()->getUsername(),
+                        '--password',
+                        $this->getRequest()->getPassword(),
+                        $url,
+                        $dir
+                    );
                 }
             );
         }
 
         if (!$this->hasError()) {
-            $this->setResponseMessage("SVN path '{$url}' created. You can now check it out locally.");
+            $this->setResponseMessage("SVN path '{$url}' is created and has been checked out locally.");
         }
 
         return $this;
