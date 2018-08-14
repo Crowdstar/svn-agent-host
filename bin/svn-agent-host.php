2e903088d924b78096299d8d6573cc35b13c235c
Demin Yin <demin.yin@glu.com>
2018-08-14 10:22:29 -0700

    use the Apache-2.0 license

--- bin/svn-agent-host.php
+++ bin/svn-agent-host.php
@@ -1,5 +1,21 @@
 #!/usr/bin/env php
 <?php
+/**************************************************************************
+ * Copyright 2018 Glu Mobile Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *************************************************************************/
+
 /**
  * This native messaging host borrows ideas from following two URLs:
  * @see https://github.com/landor/matchcommand chrome extension to run system commands from text matched in sites
